f4eea35cb29a63f5570368c2c704d7977ef7f307
Christopher Ochsenreither <replmade@gmail.com>
2023-04-15 20:36:05 -0700

    initial commit

---
+++ functions/web/home.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const ejs = require('ejs');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.set('partials', './partials');
+
+app.use(express.json());
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.get('/', (req, res) => {
+  res.render('login', {});
+});
+
+// Exporting handler for serverless deployment
+exports.handler = (event, context, callback) => {
+  const serverless = require('serverless-http');
+  const handler = serverless(app);
+  return handler(event, context, callback);
+};
